c72c2a795381123485ad859fa58d35d985071873
Tochka <imeba005@gmail.com>
2026-02-20 20:39:40 +0300

    creat counters,end

--- GameSprite.py
+++ GameSprite.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+
 class GameSprite(sprite.Sprite):
     def __init__(self,x,y,picture,speed,h,w,windows):
         self.picture = transform.scale(image.load(picture),(w,h))
@@ -22,5 +23,33 @@ class Player(GameSprite):
             self.rect.y -= 10
         if key_press[K_DOWN] and self.rect.y < 415:
             self.rect.y += 10
+    def draw(self):
+        self.windows.blit(self.picture,(self.rect.x,self.rect.y))
+class Ball(GameSprite):
+    cont1 = 0
+    cont2 = 0
+    speed_x = 2
+    speed_y = 2
+    def moving(self):
+        self.rect.x -= self.speed_x
+        self.rect.y -= self.speed_y
+    def o(self,pl1,pl2):
+        if self.rect.y < 475:
+            self.speed_y *= -1
+        if self.rect.y > 5:
+            self.speed_y *= -1
+        sprites_list = sprite.collide_rect(self,pl1)
+        sprites_list1 = sprite.collide_rect(self,pl2)
+        if sprites_list == True or sprites_list1 == True:
+            self.speed_x *= -1
+        if self.rect.x <= 0:
+            self.cont2 += 1
+            self.rect.x = 340
+            self.rect.y = 235
+        if self.rect.x >=700:
+            self.cont1 += 1
+            self.rect.x = 340
+            self.rect.y = 235
+
     def draw(self):
         self.windows.blit(self.picture,(self.rect.x,self.rect.y))
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from pygame import *
 from GameSprite import *
 
+font.init()
+font2 = font.Font(None,40)
+font1 = font.Font(None,60)
 window = display.set_mode((700,500))
 clock = time.Clock()
 fps = 60
 end = True
 player1 = Player(30,275,'rocket.png',4,80,10,window)
 player2 = Player(660,265,'rocket.png',4,80,10,window)
+ball = Ball(340,235,'ball.png',4,20,20,window)
+feedback = True
 while end:
     for i in event.get():
         if i.type == QUIT:
             end = False
-    window.fill((71, 158, 49))
-    player1.draw()
-    player2.draw()
-    player1.move()
-    player2.move1()
+    if feedback == True:
+        window.fill((71, 158, 49))
+        player1.draw()
+        player2.draw()
+        ball.draw()
+        player1.move()
+        player2.move1()
+        ball.moving()
+        ball.o(player1,player2)
+        counter1 = font2.render('счёт:'+str(ball.cont1),True,(40,0,255))
+        counter2 = font2.render('счёт:'+str(ball.cont2),True,(40,0,255))
+        window.blit(counter1,(10,10))
+        window.blit(counter2,(605,10))
+        if ball.cont1 == 3:
+            win1 = font1.render('Победил первый игрок',True,(40,0,255))
+            window.blit(win1,(140,235))
+            feedback = False
+        if ball.cont2 == 3:
+            win2 = font1.render('Победил второй игрок', True,(40,0,255))
+            window.blit(win2,(140,235))
+            feedback = False
     clock.tick(fps)
     display.update()
